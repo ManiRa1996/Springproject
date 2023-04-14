6bf3412f76ac8e42e440f4bc170b483529c3397d
Manisha Khodade <119966559+ManiRa1996@users.noreply.github.com>
2023-04-14 21:53:01 +0530

    Add files via upload

---
+++ AmazonEcommerceWebsite/src/main/java/Spring/AmazonEcommerceWebsiteApplication.java
@@ -0,0 +1,15 @@
+package Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonEcommerceWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonEcommerceWebsiteApplication.class, args);
+		
+		System.out.println("Welcome to Amazon Website..");
+	}
+
+}
